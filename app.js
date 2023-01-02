4a0a865f05e7e25e5502641d197451df588cb854
Jvjuino92 <jvmmutiz@gmail.com>
2023-01-02 12:04:10 +0000

    Almost working, requests are going 200!

--- app.js
+++ app.js
@@ -5,12 +5,25 @@ const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 
 const apiURL = `https://api.lyrics.ovh`
 
+const fetchSongs = term => {
+    fetch(`${apiURL}/suggest/${term}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+}
+
 form.addEventListener('submit', event => {
     event.preventDefault()
 
     const searchTerm = searchInput.value.trim()
 
-    console.log(searchTerm)
+    if (!searchTerm) {
+        songsContainer.innerHTML = `<li class="warning-message">Please insert an artist or song name</li>`
+        return
+    }
+
+    fetchSongs(searchTerm)
 })
 
-// 7:04
+// 18:22
