c361249a6ada4748b9238b77857c462e2e27b6db
Jvjuino92 <jvmmutiz@gmail.com>
2023-01-12 10:29:04 +0000

    the songs show on the page

--- app.js
+++ app.js
@@ -5,12 +5,26 @@ const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 
 const apiURL = `https://api.lyrics.ovh`
 
-const fetchSongs = term => {
-    fetch(`${apiURL}/suggest/${term}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-        })
+const insertSongsIntoPage = songsInfo => {
+    songsContainer.innerHTML = songsInfo.data.map(song => 
+        `<li class="song">
+            <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>
+            <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">See lyrics</button>
+        </li>`
+    ).join('')
+
+    if (songsInfo.prev || songsInfo.next) {
+        prevAndNextContainer.innerHTML = `
+            ${songsInfo.next ? `<button>Next</button>` : ''}
+        `
+    }
+}
+
+const fetchSongs = async term => {
+    const response = await fetch(`${apiURL}/suggest/${term}`)
+    const data = await response.json()
+    
+    insertSongsIntoPage(data)
 }
 
 form.addEventListener('submit', event => {
@@ -26,4 +40,4 @@ form.addEventListener('submit', event => {
     fetchSongs(searchTerm)
 })
 
-// 18:22
+// 39:24
